528f8b927ac411e4e19b6b504c408f8d472a85ae
Florent Fulleringer <florent.fulleringer@gmail.com>
2022-07-25 09:59:59 +0200

    Ajout d'un envoi des notifs sur un channel Discord

--- check_and_notify.php
+++ check_and_notify.php
@@ -4,54 +4,103 @@ require_once 'config.php';
 
 function notifySlack(string $message): void
 {
-    $curl = curl_init();
-    curl_setopt_array(
-        $curl,
-        [
-        CURLOPT_URL => getSlackUrl(),
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_TIMEOUT => 30,
-        CURLOPT_CUSTOMREQUEST => "POST",
-        CURLOPT_POSTFIELDS => json_encode([
-            'text' => ":x: $message",
-            'channel' => getSlackChannel()
-        ]),
-        CURLOPT_HTTPHEADER => [
-            sprintf('Authorization: Bearer %s', getSlackToken()),
-            "Content-type: application/json",
-        ]
-        ]
-    );
+	$curl = curl_init();
+	curl_setopt_array(
+		$curl,
+		[
+			CURLOPT_URL => getSlackUrl(),
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_TIMEOUT => 30,
+			CURLOPT_CUSTOMREQUEST => "POST",
+			CURLOPT_POSTFIELDS => json_encode([
+				'text' => ":x: $message",
+				'channel' => getSlackChannel()
+			]),
+			CURLOPT_HTTPHEADER => [
+				sprintf('Authorization: Bearer %s', getSlackToken()),
+				"Content-type: application/json",
+			]
+		]
+	);
 
-    curl_exec($curl);
-    $err = curl_error($curl);
-    curl_close($curl);
-    if ($err) {
-        logError($err);
-    }
+	curl_exec($curl);
+	$err = curl_error($curl);
+	curl_close($curl);
+	if ($err) {
+		logError($err);
+	}
+}
+
+function notifyDiscord(string $message): void
+{
+	$curl = curl_init();
+	curl_setopt_array(
+		$curl,
+		[
+			CURLOPT_URL => getDiscordUrl(),
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_TIMEOUT => 30,
+			CURLOPT_CUSTOMREQUEST => "POST",
+			CURLOPT_POSTFIELDS => json_encode([
+				'username' => 'Simple Health Check BOT',
+				'avatar_url' => 'https://picsum.photos/40/40',
+				'tts' => false,
+				'embeds' => [
+					[
+						'type' => 'rich',
+						'title' => 'Simple Health Check',
+						'description' => $message,
+						'timestamp' => date('c', strtotime('now')),
+						'color' => hexdec('BB2124'),
+						'fields' => []
+					]
+				]
+			]),
+			CURLOPT_HTTPHEADER => [
+				"Content-type: application/json",
+			]
+		]
+	);
+
+	curl_exec($curl);
+	$err = curl_error($curl);
+	curl_close($curl);
+	if ($err) {
+		logError($err);
+	}
 }
 
 function isServerError($statusCode): bool
 {
-    return $statusCode >= 500 && $statusCode < 600;
+	return $statusCode >= 500 && $statusCode < 600;
 }
 
 function logError(string $content): void
 {
-    file_put_contents('log/log_'.date("j.n.Y").'.log', $content, FILE_APPEND);
+	file_put_contents('log/log_' . date("j.n.Y") . '.log', $content, FILE_APPEND);
 }
 
 foreach (getEndpointToCheck() as $endpoint) {
-    $curlHandle = curl_init($endpoint);
-    curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, true);
-    $response = curl_exec($curlHandle);
+	$curlHandle = curl_init($endpoint);
+	curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, true);
+	$response = curl_exec($curlHandle);
 
-    if (curl_errno($curlHandle) || !$response) {
-        $message = sprintf("Unable to call %s, error: %s", $endpoint, curl_error($curlHandle));
-        logError($message);
-        notifySlack($message);
-    } elseif (isServerError($statusCode = curl_getinfo($curlHandle, CURLINFO_HTTP_CODE))) {
-        notifySlack(sprintf("%s seems down with status code %s", $endpoint, $statusCode));
-    }
-    curl_close($curlHandle);
+	if (curl_errno($curlHandle) || !$response) {
+		$message = sprintf("Unable to call %s, error: %s", $endpoint, curl_error($curlHandle));
+		logError($message);
+		if (isSlackActive()) {
+			notifySlack($message);
+		}
+		if (isDiscordActive()) {
+			notifyDiscord($message);
+		}
+	} elseif (isServerError($statusCode = curl_getinfo($curlHandle, CURLINFO_HTTP_CODE))) {
+		if (isSlackActive()) {
+			notifySlack(sprintf("%s seems down with status code %s", $endpoint, $statusCode));
+		}
+		if (isDiscordActive()) {
+			notifyDiscord(sprintf("%s seems down with status code %s", $endpoint, $statusCode));
+		}
+	}
+	curl_close($curlHandle);
 }
--- check_and_notify.sh
+++ check_and_notify.sh
@@ -3,19 +3,34 @@
 source config.sh
 
 notifySlack() {
-curl --request POST \
-  --url $1 \
-  --header "Authorization: Bearer $2" \
-  --header 'Content-type: application/json' \
-  --data '{
-        "channel": "'$3'",
-        "text": "'"$4"'",
-    }'
-  }
+	curl --request POST \
+	  --url $1 \
+	  --header "Authorization: Bearer $2" \
+	  --header 'Content-type: application/json' \
+	  --data '{
+			"channel": "'$3'",
+			"text": "'"$4"'",
+		}'
+}
+
+notifyDiscord() {
+	curl --request POST \
+	  --url $1 \
+	  --header 'Content-type: application/json' \
+	  --data '{
+	  	"username": "Simple Health Check BOT",
+	  	"content": "'"$2"'"
+	}'
+}
 
 for path in ${enpointToCheck[@]}; do
   status_code=$(curl --write-out %{http_code} --silent --output /dev/null ${path})
   if ([ "$status_code" -eq 000 ]) || ([ "$status_code" -ge 500 ] && [ "$status_code" -lt 600 ]) ; then
-    notifySlack ${slackUrl} ${slackToken} ${slackChannel} ":x: ${path} seems down with code status ${status_code}"
+	if $slackActive ; then
+		notifySlack ${slackUrl} ${slackToken} ${slackChannel} ":x: ${path} seems down with code status ${status_code}"
+	fi
+	if $discordActive ; then
+		notifyDiscord ${discordUrl} ":x: ${path} seems down with code status ${status_code}"
+	fi
   fi
 done
